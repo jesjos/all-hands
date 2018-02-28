572d66777592974926f985056fe66c536d0f5593
Jesper Josefsson <jesper.josefsson@gmail.com>
2018-02-28 20:43:51 +0200

    Pour in templates and stuff

---
+++ src/main/kotlin/se/jesjos/allhands/AllHandsApplication.kt
@@ -0,0 +1,11 @@
+package se.jesjos.allhands
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AllHandsApplication
+
+fun main(args: Array<String>) {
+    runApplication<AllHandsApplication>(*args)
+}
